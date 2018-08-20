731fa881ec5dfac7ea65c585b78904090637054e
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-19 22:22:47 -0500

    ejercicio

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -22,6 +22,21 @@ class RoomsController < ApplicationController
     redirect_to '/'
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    
+    if @room.update(room_params)
+      redirect_to rooms_path, notice: "Modificado con exito"
+    else
+      render :edit
+    end
+    
+  end
+
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds, :guests, :price_per_night, :image_url)
